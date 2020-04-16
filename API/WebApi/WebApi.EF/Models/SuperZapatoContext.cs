4e26788c9337c724f9b654ca197b2098c8d89ec9
Pablo Daniel <pablobarbi@gmail.com>
2020-04-16 19:03:26 -0300

    se modifico el readme y se cambio los conectionstring

--- API/WebApi/WebApi.EF/Models/SuperZapatoContext.cs
+++ API/WebApi/WebApi.EF/Models/SuperZapatoContext.cs
@@ -23,7 +23,7 @@ namespace WebApi.EF.Models
             if (!optionsBuilder.IsConfigured)
             {
                 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=WEBAPI\\MS2014;Database=SuperZapato;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("");
             }
         }
 
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Presentacion
+
+**Se crearon 2 proyectos**
+
+1) **Presentacion/Api:** 
+       - Se encuentra la Api que se desarrollo en base a lo solicitado
+       - Se utilizo el metodo Databases First
+       - Se debe configurar en Presentacion\API\WebApi\WebApi.EF\Models\SuperZapatoContext.cs --> El conectionstring a la base de datos.
+
+2) **Presentacion/Desktop:** Se encuentra la aplicacion realizada en C# WinForms.
+
+3) **Databases:** Se encuentra el script para la creacion de la base de datos.
+
+
